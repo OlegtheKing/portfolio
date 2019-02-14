b62b4445213969bbb263caea72b5dd4b9a7e8b3e
OlegtheKing <newrotoc@gmail.com>
2019-02-14 15:56:30 +0200

    forgot ,

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [  # add your apps here
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
     "jobs.apps.JobsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
