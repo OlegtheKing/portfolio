547b163f0932a1f96c14d1d4c2515b6b9fb13b39
OlegtheKing <newrotoc@gmail.com>
2019-02-14 15:35:32 +0200

    Set postrge server/db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',  # setting up default db engine
+        'NAME': "portfoliodb",  # name of db
+        "USER": "postgres",  # who can access it
+        "PASSWORD": "02121998",
+        "HOST":  "localhost",  # where to find,
+        "PORT": "5432",  # port of server
     }
 }
 
